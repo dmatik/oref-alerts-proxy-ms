86ca978dae29dd03df0b373e691db29063ac74fb
dmatik <dmatik@gmail.com>
2022-05-11 12:09:39 +0300

    add removal of more special characters from json response

--- src/main/java/com/dmatik/orefalerts/service/CurrentAlertHttpRequestInterceptor.java
+++ src/main/java/com/dmatik/orefalerts/service/CurrentAlertHttpRequestInterceptor.java
@@ -45,7 +45,10 @@ public class CurrentAlertHttpRequestInterceptor implements ClientHttpRequestInte
             log.debug("Current Alert Stream: " + responseBodyString);
 
             // Remove wrong characters from response to be able to parse to JSON.
-            responseBodyString=responseBodyString.replaceAll("[\r\n\u0001\0\\x00-\\x09\\x11\\x12\\x14-\\x1F\\x7F\\x0B\\x0C\\x0E-\\x1F]","");
+            responseBodyString =
+                    responseBodyString.replaceAll("[\r\n\t\u0001\0\\x00-\\x09\\x11\\x12\\x14-\\x1F\\x7F\\x0B\\x0C\\x0E-\\x1F\\u00a0]","");
+
+            responseBodyString = responseBodyString.substring(responseBodyString.indexOf("{"));
 
             // Parse to JSON
             jsonObject = new JSONObject(responseBodyString);
